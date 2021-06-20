559dce334a4a487d8b943e01da8274ce2b86d890
BAIIF <86160635+BAIIF@users.noreply.github.com>
2021-06-21 00:57:23 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 from PIL import Image
 import pytesseract as tess
-tess.pytesseract.tesseract_cmd = r'/.apt/usr/bin/tesseract.exe'
 
 app = Flask(__name__)
 text = "s"
