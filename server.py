8d683db163e2b4a4a8e64c8fcfb906741d27ad64
BAIIF <86160635+BAIIF@users.noreply.github.com>
2021-06-20 18:59:49 +0200

    Update server.py

--- server.py
+++ server.py
@@ -4,17 +4,27 @@ import pytesseract as tess
 tess.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
 
 app = Flask(__name__)
-
+text = "s"
 @app.route("/imgt", methods=["POST", "GET"])
+
+
 def process_image():
-    file = request.files['image']
-    img = Image.open(file.stream)
-    text = tess.image_to_string(img, lang='deu')
-    return text
-    
+    global text
+    if request.method == 'POST':
+        file = request.files['image']
+        img = Image.open(file.stream)
+        text = tess.image_to_string(img, lang='deu')
+ 
+        return jsonify(text)
+
 @app.route('/')
 def index():
-    return "Hello World" 
+    global text
+    return text
+    
+    
+    
+
 
 if __name__ == "__main__":
     app.run(debug=True)
